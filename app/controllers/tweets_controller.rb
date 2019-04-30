d75e06ef3d36635eba7a0721f67bb01d686bd491
riririusei99 <riry1313@gmail.com>
2019-04-30 07:28:52 +0000

    comment out TweetsController before_action

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,6 @@
 class TweetsController < ApplicationController
 
-  before_action :move_to_index, except: [ :index, :show ]     # indexアクション以外が実行される前にhogeが実行される。
+  # before_action :move_to_index, except: [ :index, :show ]     # indexアクション以外が実行される前にhogeが実行される。
 
   def index
     @tweets = Tweet.includes(:user).order("created_at DESC").page(params[:page]).per(5)

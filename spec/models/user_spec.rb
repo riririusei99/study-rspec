1a09668861eb2e52fe9f9c0c433ce9168d40383c
riririusei99 <riry1313@gmail.com>
2019-04-30 06:22:01 +0000

    change config file and shoter spec code

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,13 +4,13 @@ describe User do
   describe '#create' do
     
     it "is invalid without a nickname" do
-      user = FactoryBot.build(:user, nickname: nil)
+      user = build(:user, nickname: nil)
       user.valid?
       expect(user.errors[:nickname]).to include("can't be blank")
     end
     
     it "is invalid without a email" do
-      user = FactoryBot.build(:user, email: nil)
+      user = build(:user, email: nil)
       user.valid?
       expect(user.errors[:email]).to include("can't be blank")
     end

4e5fbbe7f47fcf6d0ec7d53a5669bd8ea39b5292
riririusei99 <riry1313@gmail.com>
2019-04-30 07:44:56 +0000

    add #edit unit testing

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -2,4 +2,19 @@ require 'rails_helper'
 
 describe TweetsController do
 
+  describe 'GET #new' do
+    it 'renders the :new template' do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'GET #edit' do
+    it "assigns the requested contact to @tweet" do
+    end
+
+    it "renders the :edit template" do
+    end
+  end
+
 end

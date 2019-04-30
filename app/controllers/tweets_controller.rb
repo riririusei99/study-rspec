ab59fb50f2f3ebb354b5657a74d6731037f0f36e
riririusei99 <riry1313@gmail.com>
2019-04-30 04:43:44 +0000

    first commit

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,45 @@
+class TweetsController < ApplicationController
+
+  before_action :move_to_index, except: [ :index, :show ]     # indexアクション以外が実行される前にhogeが実行される。
+
+  def index
+    @tweets = Tweet.includes(:user).order("created_at DESC").page(params[:page]).per(5)
+    @tweets = Tweet.all.order('id DESC').page(params[:page]).per(5)
+  end
+
+  def new
+  end
+  
+  def create
+    Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
+  end
+  
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy if tweet.user_id == current_user.id
+  end
+  
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+  
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params) if tweet.user_id == current_user.id
+  end
+  
+  def show
+    @tweet = Tweet.find(params[:id])
+    @comments = @tweet.comments.includes(:user)
+  end 
+  
+  private
+  def tweet_params
+    params.permit(:image, :text)
+  end
+  
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+  
+end

318aa1c251e0ba29dfc5fdf3ce1f250ecf1f1382
riririusei99 <riry1313@gmail.com>
2019-04-30 07:08:03 +0000

    add Unit test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 describe User do
   describe '#create' do
     
+    it "is valid with a nickname, email, password, password_confirmation" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+    
     it "is invalid without a nickname" do
       user = build(:user, nickname: "")
       user.valid?
@@ -14,5 +19,49 @@ describe User do
       user.valid?
       expect(user.errors[:email]).to include("can't be blank")
     end
+    
+    it "is invalid without a password" do
+      user = build(:user, password: "")
+      user.valid?
+      expect(user.errors[:password]).to include("can't be blank")
+    end
+
+    it "is invalid without a password_confirmation" do
+      user = build(:user, password_confirmation: "")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("doesn't match Password")
+    end
+
+    it "is invalid nickname longer than 7 characters" do
+      user = build(:user, nickname: "longsheng")
+      user.valid?
+      expect(user.errors[:nickname]).to include("is too long (maximum is 6 characters)")
+    end
+
+    it "is valid with nickname less than 6 charaters" do
+      user = build(:user, nickname: "123456")
+      expect(user).to be_valid
+    end
+    
+    it "is invalid with a duplicate email address" do
+      #はじめにユーザーを登録
+      user = create(:user)
+      #先に登録したユーザーと同じemailの値を持つユーザーのインスタンスを作成
+      another_user = build(:user, email: user.email)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("has already been taken")
+    end
+
+    it "is invalid nickname less than 5 password" do
+      user = build(:user, password: "12345", password_confirmation: "12345")
+      user.valid?
+      expect(user.errors[:password]).to include("is too short (minimum is 6 characters)")
+    end
+
+    it "is valid with nickname longer than 6 charaters" do
+      user = build(:user, password: "123456", password_confirmation: "123456")
+      expect(user).to be_valid
+    end
+
   end
 end
